ca57478a57794d323391cac902ca33e26122c88c
harsh556 <114024228+harsh556@users.noreply.github.com>
2022-09-23 00:01:43 +0530

    Add files via upload

---
+++ rds demo project.py
@@ -0,0 +1,42 @@
+!pip install PyMySQL
+import pymysql
+db=pymysql.connect(host='database-1internship.cdjiaumwxjfa.ap-south-1.rds.amazonaws.com',user='admin',password='12345678')
+cursor = db.cursor()
+cursor
+cursor.execute("select version()")
+data
+sql = '''create database hospital'''
+cursor.execute(sql)
+cursor.connection.commit()
+sql = '''use hospital'''
+cursor.execute(sql)
+sql = '''
+create table patient(
+sno int not null auto_increment primary key,
+pname text,
+page text,
+psex text,
+pBgroup text,
+pdoctor text,
+pbill text
+)'''
+cursor.execute(sql)
+sql = '''show tables'''
+cursor.execute(sql)
+cursor.fetchall()
+sql = '''
+insert into patient(pname, page, pBgroup, pdoctor, pbill)values('%s', '%s', '%s', '%s', '%s')'''%('harsh', '22', 'b+', 'Drvijay', 'two hundred')
+cursor.execute(sql)
+db.commit()
+sql = '''select * from patient'''
+cursor.execute(sql)
+cursor.fetchall()
+    option = input('to see patient details press 1\n')
+    if option=='1':
+            sql = '''select * from patient'''
+            a= cursor.execute(sql)
+            b= cursor.fetchall()
+            print (b)
+    else:
+        exit
+
